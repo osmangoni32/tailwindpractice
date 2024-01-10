978b283886fd4f43670bf4f37c2056b550a47110
osman goni <osman.ice.32@gmail.com>
2024-01-10 16:12:11 +0600

    initialized the project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <body className="grid grid-cols-12 grid-rows-[80vh,min-content,40vw,min-content,min-content,min-content] gap-1 p-2 bg-white ">
+    <div className="bg-orange-500 row-span-full col-span-1"></div>
+        <div className="bg-red-600 row-span-1  col-start-2 col-end-10 ">k</div>
+        <div className='bg-gray-400 row-span-1 col-start-10 col-end-13'>hh</div>
+    <div className="bg-blue-600 row-span-1 col-span-11 ">m</div>
+    <div className=" bg-green-500 row-span-1 col-span-11" ></div>
+    <div className=" bg-green-900 row-span-1 col-span-11"  >h</div>
+    <div className=" bg-green-500 row-span-1 col-span-11" >hgcgh</div>
+    <div className=" bg-green-500 row-span-1 col-span-11" >hgcgh</div>
+    
+   
+      </body>
+      </>
+  );
+}
+
+export default App;
